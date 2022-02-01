81db1c267d953cea6ac9c9ee88d041b664de97b2
fannijako <fannijako@gmail.com>
2022-02-01 11:16:25 +0100

    title scraper function

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from beautifulsoup4 import BeautifulSoup
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 
@@ -35,9 +35,19 @@ def create_soup(url, sub_1):
     ''' creating a html soup from a html sites subsite'''
     link = url + sub_1
     html_page = urlopen(link)
-    soup = BeautifulSoup(html_page, 'lxml')
+    soup = BeautifulSoup(html_page)
 
     return soup
 
 # test with the first film from the top250 list
 soup = create_soup('https://www.imdb.com', link_list[0])
+
+def title_scraper(soup):
+    '''get the title of the movie from a BeautifulSoup object of an IMDB site'''
+
+    s_title = soup.findAll('h1', attrs={'class': re.compile('TitleHead')})
+
+    return s_title[0].string
+
+# test the first film from the top250 list
+title = title_scraper(soup)
